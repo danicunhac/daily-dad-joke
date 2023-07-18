66304d6ba729b41b46cbf33be4b5c92f1cb9dcae
Daniel Cunha <danielcc.jp@gmail.com>
2023-07-18 10:26:31 -0300

    refac: update prompt for the rest of the month

--- utils/index.ts
+++ utils/index.ts
@@ -21,7 +21,7 @@ export async function getJoke(): Promise<string> {
     .createCompletion({
       model: 'text-davinci-003',
       prompt:
-        'Tell me a dad joke. The answer should be structured in json format, like this: { "question": QUESTION, "answer": ANSWER }',
+        'Tell me a dad joke related to the work environment. The answer should be structured in json format, like this: { "question": QUESTION, "answer": ANSWER }',
       max_tokens: 60,
       temperature: 1,
       top_p: 1,
