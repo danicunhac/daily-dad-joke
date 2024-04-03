452edd130f73b00c71e743193918e45da8a372af
Daniel Cunha <danielcc.jp@gmail.com>
2024-04-03 10:47:43 -0300

    fix: add limit

--- utils/index.ts
+++ utils/index.ts
@@ -141,6 +141,7 @@ export async function getExistingJokes(fields?: string): Promise<Joke[]> {
       .from('jokes')
       .select(fields || '*')
       .order('created_at', { ascending: false })
+      .limit(100)
       .neq('created_at', currentDate)) as unknown as { data: Joke[] };
 
     const mappedJokes = data.map((joke) => {
