524d73404b7c127bcaf28af3458ce24192aca2a8
Daniel Cunha <danielcc.jp@gmail.com>
2024-04-15 20:25:18 -0300

    fix: remove subscription

--- app/page.tsx
+++ app/page.tsx
@@ -154,36 +154,6 @@ export default function Home() {
           )}
         </div>
       </section>
-
-      <section id="subscribe" className="pt-8 px-16 flex-1 w-full">
-        <div className="flex items-center justify-center [&amp;>div]:w-full">
-          <div className="rounded-xl border bg-card text-card-foreground shadow">
-            <div className="flex flex-col space-y-1.5 p-6">
-              <h3 className="font-semibold leading-none tracking-tight">
-                Subscribe to receive Daily Dad Jokes!
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                They are free and funny.
-              </p>
-            </div>
-            <div className="p-6 pt-0">
-              <div className="flex space-x-2">
-                <input
-                  className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-                  placeholder="daniel@email.com"
-                />
-                <button
-                  onClick={() => {}}
-                  className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 shrink-0"
-                >
-                  Subscribe
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
       <section id="jokes" className="py-12 px-16 flex-1 w-full">
         {Array.isArray(jokes) ? (
           <ol className="grid gap-24 grid-cols-3 max-[900px]:grid-cols-2 max-[580px]:grid-cols-1">
