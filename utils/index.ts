db297082e96239cfcc9c99d86c76cfb820c3a468
Daniel Cunha <danielcc.jp@gmail.com>
2024-05-16 22:59:36 -0300

    update: gpt-4o (hope its good)

--- utils/index.ts
+++ utils/index.ts
@@ -28,7 +28,7 @@ export async function getJoke(
     : prompt;
 
   const { choices } = await openai.chat.completions.create({
-    model: 'gpt-4',
+    model: 'gpt-4o',
     messages: [
       {
         role: 'system',
